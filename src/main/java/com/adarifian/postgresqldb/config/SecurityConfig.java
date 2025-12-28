8ee4d3428729f3c876bb66358545523f14bad7e7
anandadwiarifian <anandadwiarifin@gmail.com>
2025-12-29 00:01:23 +0800

    add security configuration with basic authentication and update README

---
+++ src/main/java/com/adarifian/postgresqldb/config/SecurityConfig.java
@@ -0,0 +1,63 @@
+package com.adarifian.postgresqldb.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+
+public class SecurityConfig {
+
+    @Value("${security.user.password}")
+    private String userPassword;
+
+    @Value("${security.admin.password}")
+    private String adminPassword;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize.requestMatchers("/healthcheck").permitAll()
+                        .requestMatchers(HttpMethod.DELETE).hasRole("ADMIN")
+                        .requestMatchers("/authors/**", "/books/**").hasAnyRole("ADMIN", "USER")
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults()).csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        UserDetails user = User.builder()
+                .username("user")
+                .password(passwordEncoder().encode(userPassword))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode(adminPassword))
+                .roles("ADMIN", "USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
